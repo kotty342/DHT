44aa2f634a5cc11fad8e74ed04e4eecde74394e2
kotty342 <aa21007@cc.it-hiroshima.ac.jp>
2024-12-16 14:50:54 +0000

    DHT構造体とノード管理機能を追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use std::net::SocketAddr;
+
+#[derive(Clone)]
+struct Node {
+    id: String,
+    address: SocketAddr,
+}
+
+struct DHT {
+    nodes: Arc<Mutex<HashMap<String, Node>>>,
+}
+
+impl DHT {
+    fn new() -> Self {
+        DHT {
+            nodes: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+
+    fn add_node(&self, id: String, address: SocketAddr) {
+        let mut nodes = self.nodes.lock().unwrap();
+        nodes.insert(id.clone(), Node { id, address });
+    }
+
+    fn get_node(&self, id: &str) -> Option<Node> {
+        let nodes = self.nodes.lock().unwrap();
+        nodes.get(id).cloned()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let dht = DHT::new();
+    let node_id = "node1".to_string();
+    let node_address: SocketAddr = "127.0.0.1:8080".parse().unwrap();
+
+    dht.add_node(node_id.clone(), node_address);
+
+    if let Some(node) = dht.get_node(&node_id) {
+        println!("Found node: {} at {}", node.id, node.address);
+    } else {
+        println!("Node not found");
+    }
 }
